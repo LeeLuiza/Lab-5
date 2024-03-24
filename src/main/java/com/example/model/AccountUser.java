59ff69cf98372882b620247cdc0cd2a866fb2af5
LeeLuiza <Laurabeck1315@gmail.com>
2024-03-25 03:34:42 +0500

    first commit

---
+++ src/main/java/com/example/model/AccountUser.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+public class AccountUser {
+    private String login;
+    private String email;
+    private String password;
+
+    public AccountUser(String login, String password, String email){
+        this.login = login;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
